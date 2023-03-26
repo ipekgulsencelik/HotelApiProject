30027b0e9defa1dd428d86fb56d66bb93a562227
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-26 14:29:25 +0300

    adding services

---
+++ ApiConsume/HotelApiProject.BusinessLayer/Abstract/ITestimonialService.cs
@@ -0,0 +1,8 @@
+﻿using HotelApiProject.EntityLayer.Concrete;
+
+namespace HotelApiProject.BusinessLayer.Abstract
+{
+    public interface ITestimonialService : IGenericService<Testimonial>
+    {
+    }
+}
