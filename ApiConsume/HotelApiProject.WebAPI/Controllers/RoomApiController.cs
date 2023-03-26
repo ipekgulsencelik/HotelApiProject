f44764d0402668e37e75435610f1867deee6ec41
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-27 02:08:02 +0300

    checking mapper for room

---
+++ ApiConsume/HotelApiProject.WebAPI/Controllers/RoomApiController.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using HotelApiProject.BusinessLayer.Abstract;
+using HotelApiProject.DataTransferObjectLayer.DTOs.RoomDTO;
+using HotelApiProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelApiProject.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomApiController : ControllerBase
+    {
+        private readonly IRoomService _roomService;
+        private readonly IMapper _mapper;
+        
+        public RoomApiController(IRoomService roomService, IMapper mapper)
+        {
+            _roomService = roomService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var values = _roomService.TGetList();
+            return Ok(values);
+        }
+        
+        [HttpPost]
+        public IActionResult AddRoom(RoomAddDTO roomAddDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var values = _mapper.Map<Room>(roomAddDTO);
+            _roomService.TInsert(values);
+            
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateRoom(UpdateRoomDTO updateRoomDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            
+            var values = _mapper.Map<Room>(updateRoomDTO);
+            _roomService.TUpdate(values);
+            
+            return Ok("Successfully Updated");
+        }
+    }
+}
--- ApiConsume/HotelApiProject.WebAPI/Mapping/AutoMapperConfig.cs
+++ ApiConsume/HotelApiProject.WebAPI/Mapping/AutoMapperConfig.cs
@@ -1,6 +1,17 @@
-﻿namespace HotelApiProject.WebAPI.Mapping
+﻿using AutoMapper;
+using HotelApiProject.DataTransferObjectLayer.DTOs.RoomDTO;
+using HotelApiProject.EntityLayer.Concrete;
+
+namespace HotelApiProject.WebAPI.Mapping
 {
-    public class AutoMapperConfig
+    public class AutoMapperConfig : Profile
     {
+        public AutoMapperConfig()
+        {
+            CreateMap<RoomAddDTO, Room>();
+            CreateMap<Room, RoomAddDTO>();
+
+            CreateMap<UpdateRoomDTO, Room>().ReverseMap();
+        }
     }
 }
