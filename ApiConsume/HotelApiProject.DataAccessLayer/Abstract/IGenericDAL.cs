2c9357e1a1c23f7075712cc190f30360639b26d9
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-26 13:36:12 +0300

    adding abstract interface

---
+++ ApiConsume/HotelApiProject.DataAccessLayer/Abstract/IGenericDAL.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelApiProject.DataAccessLayer.Abstract
+{
+    public interface IGenericDAL<T> where T : class
+    {
+        void Insert(T t);
+        void Delete(T t);
+        void Update(T t);
+        List<T> GetList();
+        T GetByID(int id);
+    }
+}
