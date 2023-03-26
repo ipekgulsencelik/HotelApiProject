ba76f06b62f5c45d8012cabc0f6f61588c2bf9a4
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-26 19:04:52 +0300

    adding controller classes

---
+++ ApiConsume/HotelApiProject.WebAPI/Controllers/TestimonialController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelApiProject.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialController : ControllerBase
+    {
+    }
+}
--- ApiConsume/HotelApiProject.WebAPI/Program.cs
+++ ApiConsume/HotelApiProject.WebAPI/Program.cs
@@ -1,3 +1,8 @@
+using HotelApiProject.BusinessLayer.Abstract;
+using HotelApiProject.BusinessLayer.Concrete;
+using HotelApiProject.DataAccessLayer.Abstract;
+using HotelApiProject.DataAccessLayer.Concrete;
+using HotelApiProject.DataAccessLayer.Concrete.EntityFramework;
 
 namespace HotelApiProject.WebAPI
 {
@@ -8,6 +13,23 @@ namespace HotelApiProject.WebAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
+            builder.Services.AddDbContext<Context>();
+
+            builder.Services.AddScoped<IRoomDAL, EFRoomDAL>();
+            builder.Services.AddScoped<IRoomService, RoomManager>();
+
+            builder.Services.AddScoped<IServiceDAL, EFServiceDAL>();
+            builder.Services.AddScoped<IServiceService, ServiceManager>();
+
+            builder.Services.AddScoped<IStaffDAL, EFStaffDAL>();
+            builder.Services.AddScoped<IStaffService, StaffManager>();
+
+            builder.Services.AddScoped<ISubscribeDAL, EFSubscribeDAL>();
+            builder.Services.AddScoped<ISubscribeService, SubscribeManager>();
+
+            builder.Services.AddScoped<ITestimonialDAL, EFTestimonialDAL>();
+            builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
