2849e923dbc246a5a38f308eef1eeb08d5be869a
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-26 23:11:18 +0300

    adding admin layout with partial views

--- ApiConsume/HotelApiProject.WebAPI/Controllers/SubscribeController.cs
+++ ApiConsume/HotelApiProject.WebAPI/Controllers/SubscribeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HotelApiProject.BusinessLayer.Abstract;
+using HotelApiProject.EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HotelApiProject.WebAPI.Controllers
@@ -7,5 +8,47 @@ namespace HotelApiProject.WebAPI.Controllers
     [ApiController]
     public class SubscribeController : ControllerBase
     {
+        private readonly ISubscribeService _subscribeService;
+        
+        public SubscribeController(ISubscribeService subscribeService)
+        {
+            _subscribeService = subscribeService;
+        }
+
+        [HttpGet]
+        public IActionResult SubscribeList()
+        {
+            var values = _subscribeService.TGetList();
+            return Ok(values);
+        }
+        
+        [HttpPost]
+        public IActionResult AddSubscribe(Subscribe subscribe)
+        {
+            _subscribeService.TInsert(subscribe);
+            return Ok();
+        }
+        
+        [HttpDelete]
+        public IActionResult DeleteSubscribe(int id)
+        {
+            var values = _subscribeService.TGetByID(id);
+            _subscribeService.TDelete(values);
+            return Ok();
+        }
+        
+        [HttpPut]
+        public IActionResult UpdateSubscribe(Subscribe subscribe)
+        {
+            _subscribeService.TUpdate(subscribe);
+            return Ok();
+        }
+        
+        [HttpGet("{id}")]
+        public IActionResult GetSubscribe(int id)
+        {
+            var values = _subscribeService.TGetByID(id);
+            return Ok(values);
+        }
     }
 }
--- ApiConsume/HotelApiProject.WebAPI/Controllers/TestimonialController.cs
+++ ApiConsume/HotelApiProject.WebAPI/Controllers/TestimonialController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HotelApiProject.BusinessLayer.Abstract;
+using HotelApiProject.EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HotelApiProject.WebAPI.Controllers
@@ -7,5 +8,47 @@ namespace HotelApiProject.WebAPI.Controllers
     [ApiController]
     public class TestimonialController : ControllerBase
     {
+        private readonly ITestimonialService _testimonialService;
+        
+        public TestimonialController(ITestimonialService testimonialService)
+        {
+            _testimonialService = testimonialService;
+        }
+
+        [HttpGet]
+        public IActionResult TestimonialList()
+        {
+            var values = _testimonialService.TGetList();
+            return Ok(values);
+        }
+        
+        [HttpPost]
+        public IActionResult AddTestimonial(Testimonial testimonial)
+        {
+            _testimonialService.TInsert(testimonial);
+            return Ok();
+        }
+        
+        [HttpDelete("{id}")]
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var values = _testimonialService.TGetByID(id);
+            _testimonialService.TDelete(values);
+            return Ok();
+        }
+        
+        [HttpPut]
+        public IActionResult UpdateTestimonial(Testimonial testimonial)
+        {
+            _testimonialService.TUpdate(testimonial);
+            return Ok();
+        }
+        
+        [HttpGet("{id}")]
+        public IActionResult GetTestimonial(int id)
+        {
+            var values = _testimonialService.TGetByID(id);
+            return Ok(values);
+        }
     }
 }
