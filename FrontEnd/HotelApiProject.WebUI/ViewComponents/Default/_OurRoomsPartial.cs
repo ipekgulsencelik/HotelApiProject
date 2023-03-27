75b50618f73fad76b7f8203bfd3abf7f3554f2f8
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-28 02:01:05 +0300

    adding room page

--- FrontEnd/HotelApiProject.WebUI/ViewComponents/Default/_OurRoomsPartial.cs
+++ FrontEnd/HotelApiProject.WebUI/ViewComponents/Default/_OurRoomsPartial.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelApiProject.WebUI.DTOs.RoomDTO;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelApiProject.WebUI.ViewComponents.Default
 {
     public class _OurRoomsPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        
+        public _OurRoomsPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5000/api/Room");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultRoomDTO>>(jsonData);
+
+                return View(values);
+            }
+
             return View();
         }
     }
