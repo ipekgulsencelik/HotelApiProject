64f497834337949cdc761f4db458ff4efed13380
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-27 16:04:57 +0300

    updating service

--- FrontEnd/HotelApiProject.WebUI/Controllers/ServiceController.cs
+++ FrontEnd/HotelApiProject.WebUI/Controllers/ServiceController.cs
@@ -66,5 +66,41 @@ namespace HotelApiProject.WebUI.Controllers
             
             return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateService(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:5000/api/Service/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateServiceDTO>(jsonData);
+            
+                return View(values);
+            }
+            
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateService(UpdateServiceDTO updateServiceDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateServiceDTO);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("http://localhost:5000/api/Service/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+         
+            return View();
+        }
     }
 }
