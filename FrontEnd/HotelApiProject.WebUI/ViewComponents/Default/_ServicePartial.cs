ff770e9ded3c37d55da25d771382592a79234ecc
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-29 20:29:25 +0300

    adding service page

--- FrontEnd/HotelApiProject.WebUI/ViewComponents/Default/_ServicePartial.cs
+++ FrontEnd/HotelApiProject.WebUI/ViewComponents/Default/_ServicePartial.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelApiProject.WebUI.DTOs.ServiceDTO;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelApiProject.WebUI.ViewComponents.Default
 {
     public class _ServicePartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        
+        public _ServicePartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5000/api/Service");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDTO>>(jsonData);
+
+                return View(values);
+            }
+
             return View();
         }
     }
