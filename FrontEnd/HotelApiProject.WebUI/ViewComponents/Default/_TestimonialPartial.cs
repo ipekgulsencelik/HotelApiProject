74383cf6f73108ba9608ae2e645191e1dc0da85e
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-03-29 20:38:17 +0300

    adding testimonial page

--- FrontEnd/HotelApiProject.WebUI/ViewComponents/Default/_TestimonialPartial.cs
+++ FrontEnd/HotelApiProject.WebUI/ViewComponents/Default/_TestimonialPartial.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HotelApiProject.WebUI.DTOs.TestimonialDTO;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace HotelApiProject.WebUI.ViewComponents.Default
 {
     public class _TestimonialPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        
+        public _TestimonialPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5000/api/Testimonial");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDTO>>(jsonData);
+        
+                return View(values);
+            }
+            
             return View();
         }
     }
