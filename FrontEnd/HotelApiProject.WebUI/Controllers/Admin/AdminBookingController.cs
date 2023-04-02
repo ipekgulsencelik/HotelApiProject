21251278f7302e15a859e7e3aad3ae67be063ee7
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-02 23:18:18 +0300

    adding booking list for admin dashboard

---
+++ FrontEnd/HotelApiProject.WebUI/Controllers/Admin/AdminBookingController.cs
@@ -0,0 +1,31 @@
+﻿using HotelApiProject.WebUI.DTOs.BookingDTO;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HotelApiProject.WebUI.Controllers.Admin
+{
+    public class AdminBookingController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        
+        public AdminBookingController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5000/api/Booking");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBookingDTO>>(jsonData);
+
+                return View(values);
+            }
+            
+            return View();
+        }
+    }
+}
