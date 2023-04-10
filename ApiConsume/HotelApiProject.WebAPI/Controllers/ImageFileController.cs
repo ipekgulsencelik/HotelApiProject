835f772615c05f55fd2b10e27766b75db1cdd4b7
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2023-04-10 16:08:13 +0300

    processing file

---
+++ ApiConsume/HotelApiProject.WebAPI/Controllers/ImageFileController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelApiProject.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageFileController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> UploadImage([FromForm] IFormFile file)
+        {
+            var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "Images/" + fileName);
+            var stream = new FileStream(path, FileMode.Create);
+            await file.CopyToAsync(stream);
+
+            return Created("", file);
+        }
+    }
+}
